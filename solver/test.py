30a42796ec69b9b4df68da6c19fc7c617db637e0
BavoGoosens <bavogoosens@hotmail.com>
2016-03-16 15:49:34 +0100

    linked list

--- solver/test.py
+++ solver/test.py
@@ -1,17 +1,29 @@
 from generator import Generator
 from linked_list import LinkedList
 
+possibles = list()
+
 def flatten(element, ll):
-    for el in element:
-        if isinstance(el, list):
+        if isinstance(element, list):
+            if len(element) == 0:
+                print(ll.couple())
+                return
             print("sup")
-            flatten(el, ll)
-        elif isinstance(el, tuple):
-            element[0]
-            element[1]
-        elif isinstance(el, int):
-            ll.add(el)
-    return ll
+            head = element[0]
+            tail = element[1]
+            ll.add(head)
+            flatten(tail, ll)
+        elif isinstance(element, tuple):
+            # splits de linked list
+            fst = element[0]
+            snd = element[1]
+            newll1 = LinkedList(ll)
+            newll2 = LinkedList(ll)
+            flatten(fst, newll1)
+            flatten(snd, newll2)
+        elif isinstance(element, int):
+            ll.add(element)
+
 
 generator = Generator(6, 3, 5, 2, 3)
 solutions = generator.generate_solutions(5)
@@ -20,5 +32,5 @@ ll = LinkedList()
 flatten(solutions, ll)
 
 
-print solutions
+print(solutions)
 
