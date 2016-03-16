36cf3de271268d58044b85568a0d8650a22b6eec
= <michielvdd@me.com>
2016-03-16 16:17:09 +0100

    stuff

--- solver/linked_list.py
+++ solver/linked_list.py
@@ -11,7 +11,8 @@ class LinkedList(object):
             return self.currentList
         else:
             coupled_list = self.previous.couple()
-            return coupled_list.append(self.currentList)
+            coupled_list.append(self.currentList)
+            return coupled_list
 
     def add(self, entry):
         self.currentList.append(entry)
